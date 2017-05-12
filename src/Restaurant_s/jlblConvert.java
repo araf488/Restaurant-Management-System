4542410d8604fde9cdbce685173b1bf0011ff435
araf488 <araf488@gmail.com>
2017-05-12 16:25:14 +0600

    This is a JAVAFX, Spring, Hibernate, MySQL project

---
+++ src/Restaurant_s/jlblConvert.java
@@ -0,0 +1,9 @@
+package Restaurant_s;
+
+class jlblConvert {
+
+    static void setText(String cConvert1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
